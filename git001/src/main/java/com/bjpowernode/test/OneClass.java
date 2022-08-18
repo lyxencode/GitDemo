d3fdca33e5dfbffc13226f8e8d3f17a06e7cb1fb
lyxencode <919663644@qq.com>
2022-08-18 19:52:24 +0800

    第二次增加一个类OneClass

---
+++ git001/src/main/java/com/bjpowernode/test/OneClass.java
@@ -0,0 +1,9 @@
+package com.bjpowernode.test;
+
+import java.sql.SQLOutput;
+
+public class OneClass {
+    public static void main(String[] args) {
+        System.out.println("第一个java模块");
+    }
+}
